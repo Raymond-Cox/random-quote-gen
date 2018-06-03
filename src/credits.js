6a88f60075c1df6eb4fc21182354f0c98750bc16
Raymond Cox <raymondc1911@gmail.com>
2018-06-03 17:13:49 -0400

    Base skeleton complete
    -Main layout framed up

---
+++ src/credits.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Credits = () => {
+  return (
+    <div>
+      <p>Created by <a href="https://github.com/Raymond-Cox" target="_blank" rel="noopener noreferrer"> Raymond Cox</a></p>
+      <p>Quotes from <a href="https://www.huffingtonpost.com/lolly-daskal-/100-motivational-quotes-t_b_4505356.html" target="_blank" rel="noopener noreferrer">Huffington Post</a></p>
+      <p>Image from <a href="https://unsplash.com/@garciasaldana_" target="_blank" rel="noopener noreferrer">Pablo García Saldaña</a></p>
+    </div>
+  )
+}
+
+export default Credits;
